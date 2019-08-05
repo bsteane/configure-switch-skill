5cf2200108cb0105d1b8fc6929bc7ec144ff9c4b
bsteane <52600617+bsteane@users.noreply.github.com>
2019-08-05 13:53:36 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,37 @@
-from mycroft import MycroftSkill, intent_file_handler
+import paramiko 
+import time 
+import getpass 
+import os 
 
+#from host_file import network_devices 
+#from config_file import host_conf
+from mycroft import MycroftSkill, intent_file_handler 
 
-class ConfigureSwitch(MycroftSkill):
-    def __init__(self):
-        MycroftSkill.__init__(self)
+__author__ = "bsteane" 
 
-    @intent_file_handler('switch.configure.intent')
-    def handle_switch_configure(self, message):
-        self.speak_dialog('switch.configure')
+class ConfigureSwitch(MycroftSkill): 
+    
+    def __init__(self): 
+        MycroftSkill.__init__(self) 
 
-
-def create_skill():
+    @intent_file_handler('switch.configure.intent') 
+    def handle_switch_configure(self, message): 
+        ip = "192.168.0.200" 
+        twrssh = paramiko.SSHClient() 
+        twrssh.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
+        twrssh.connect(ip, port=22, username="cisco", password="cisco") 
+        remote = twrssh.invoke_shell() 
+        remote.send('term len 0\n')
+        remote.send('en\n') 
+        remote.send('cisco\n')
+        remote.send('conf t\n')
+        remote.send('int g0/1\n') 
+        remote.send('desc ben your in\n') 
+        time.sleep(1) 
+        twrssh.close()
+        self.speak_dialog('switch.configure') 
+ 
+def create_skill(): 
     return ConfigureSwitch()
+
 
